937e922d9d42774a8ecb6b04bc8a1096e8b58cea
Erkka O Kaaria <erkka.kaaria@hotmail.com>
2014-05-14 14:19:01 +0300

    Added settings menu button and handler

---
+++ eclipse-plugin/src/tmc/handlers/SettingsMenuHandler.java
@@ -0,0 +1,22 @@
+package tmc.handlers;
+
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+
+public class SettingsMenuHandler extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Settings",
+				"Settings menu to be added here");
+		return null;
+	}
+}
