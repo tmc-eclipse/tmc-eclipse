ca01df7df8c1b5ea2009f12590d5d9428f18a55a
veepee <villepekkah@gmail.com>
2014-05-14 14:27:08 +0300

    Mock tests for Settings

---
+++ plugin-logic/src/test/java/fi/helsinki/cs/plugin/tmc/model/SettingsTest.java
@@ -0,0 +1,42 @@
+package fi.helsinki.cs.plugin.tmc.model;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.prefs.Preferences;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SettingsTest {
+
+	Preferences prefs;
+	Settings settings;
+	
+	@Before
+	public void setUp() {
+		prefs = mock(Preferences.class);
+		settings = new Settings(prefs);
+	}
+	
+	@Test
+	public void testSavingServerBaseUrl() {
+		settings.setServerBaseUrl("http://tmc.mooc.fi");
+		
+		verify(prefs).put(eq(Settings.PREF_KEY_TMC_SERVER_URL), eq("http://tmc.mooc.fi"));
+	}
+
+	@Test
+	public void testSavingUsername() {
+		settings.setUsername("matti.meikalainen");
+		
+		verify(prefs).put(eq(Settings.PREF_KEY_USERNAME), eq("matti.meikalainen"));
+	}
+	
+	@Test
+	public void testSavingPassword() {
+		settings.setPassword("password123");
+		
+		verify(prefs).put(eq(Settings.PREF_KEY_PASSWORD), eq("password123"));
+	}
+}
