525d1082428ffa83d919b6abc55345f4552ce5fc
Nikke S Kostiainen <nkostiai@dat-14.cs.helsinki.fi>
2014-05-14 15:01:53 +0300

    added settings window

---
+++ eclipse-plugin/src/tmc/handlers/SettingsMenuHandler.java
@@ -0,0 +1,21 @@
+package tmc.handlers;
+
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import tmc.ui.SettingsWindow;
+
+
+public class SettingsMenuHandler extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		SettingsWindow modal = SettingsWindow.getNewModal(window.getShell());
+		modal.open();
+		return null;
+	}
+}
