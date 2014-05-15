8330ae0d44c161d7aefe8bf4b8d4097051b4334b
Erkka O Kaaria <erkka_kaaria@hotmail.com>
2014-05-15 12:00:30 +0300

    Settings and login prompt windows

--- eclipse-plugin/src/tmc/handlers/SettingsMenuHandler.java
+++ eclipse-plugin/src/tmc/handlers/SettingsMenuHandler.java
@@ -4,17 +4,17 @@ package tmc.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.SWT;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import tmc.ui.SettingsDialog;
-import org.eclipse.swt.SWT;
-
 
 
 public class SettingsMenuHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+
 		SettingsDialog dialog = new SettingsDialog(window.getShell(), SWT.SHEET);
 		dialog.open();
 		return null;
