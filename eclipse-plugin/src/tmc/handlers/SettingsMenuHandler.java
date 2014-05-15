61608cbde55d673bb1e2c381cf5d836b53eb8bee
Erkka O Kaaria <erkka_kaaria@hotmail.com>
2014-05-15 11:45:16 +0300

    Merged initial settings dialog

--- eclipse-plugin/src/tmc/handlers/SettingsMenuHandler.java
+++ eclipse-plugin/src/tmc/handlers/SettingsMenuHandler.java
@@ -6,14 +6,17 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import tmc.ui.SettingsWindow;
+import tmc.ui.SettingsDialog;
+import org.eclipse.swt.SWT;
+
+
 
 public class SettingsMenuHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		SettingsWindow modal = SettingsWindow.getNewModal(window.getShell());
-		modal.open();
+		SettingsDialog dialog = new SettingsDialog(window.getShell(), SWT.SHEET);
+		dialog.open();
 		return null;
 	}
 }
