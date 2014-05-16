97495fcbc48596d50b6d3750f71358ed57071c4f
Juhani P Helio <jphelio@dat-5.cs.helsinki.fi>
2014-05-16 13:47:49 +0300

    GsonAdapter Tests

--- plugin-logic/src/test/java/fi/helsinki/cs/plugin/domain/exerciseKeyTest.java
+++ plugin-logic/src/test/java/fi/helsinki/cs/plugin/domain/exerciseKeyTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+
 import fi.helsinki.cs.plugin.tmc.domain.Exercise;
 import fi.helsinki.cs.plugin.tmc.domain.ExerciseKey;
 
@@ -46,5 +49,32 @@ public class exerciseKeyTest {
 	public void testToString() {
 		assertEquals(ek.toString(), "asd/asd");
 	}
-
+	
+	@Test
+	public void testGsonAdapterSerialize(){
+		ExerciseKey.GsonAdapter ga=new ExerciseKey.GsonAdapter();
+		assertEquals(ga.serialize(ek, ek.getClass(), null), new JsonPrimitive("asd/asd"));
+	}
+	
+	@Test
+	public void testGsonAdapterDeserialize(){
+		ExerciseKey.GsonAdapter ga=new ExerciseKey.GsonAdapter();
+		JsonPrimitive jp=new JsonPrimitive("asd/asd");
+		assertEquals(ga.deserialize(jp, jp.getClass(), null), ek);
+	}
+	
+	@Test(expected=JsonParseException.class)
+	public void testGsonAdapterDeserializeFailure() throws JsonParseException{
+		ExerciseKey.GsonAdapter ga=new ExerciseKey.GsonAdapter();
+		JsonPrimitive jp=new JsonPrimitive("asdasdasd");
+		ga.deserialize(jp, jp.getClass(), null);
+	}
+	
+	@Test(expected=JsonParseException.class)
+	public void testGsonAdapterDeserializeFailureEmptyString() throws JsonParseException{
+		ExerciseKey.GsonAdapter ga=new ExerciseKey.GsonAdapter();
+		JsonPrimitive jp=new JsonPrimitive("");
+		ga.deserialize(jp, jp.getClass(), null);
+	}
+	
 }
