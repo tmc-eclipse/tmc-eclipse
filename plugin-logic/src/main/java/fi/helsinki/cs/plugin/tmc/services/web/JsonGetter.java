5bcf7a6fe27937b00d442910d1d9e4db69977b08
veepee <villepekkah@gmail.com>
2014-05-19 15:40:21 +0300

    Refactored JsonGetter

--- plugin-logic/src/main/java/fi/helsinki/cs/plugin/tmc/services/web/JsonGetter.java
+++ plugin-logic/src/main/java/fi/helsinki/cs/plugin/tmc/services/web/JsonGetter.java
@@ -1,28 +1,21 @@
 package fi.helsinki.cs.plugin.tmc.services.web;
 
-import java.io.InputStream;
-
 import org.apache.commons.io.IOUtils;
-
 import fi.helsinki.cs.plugin.tmc.services.http.HttpRequestExecutor;
-import fi.helsinki.cs.plugin.tmc.services.http.HttpTasks;
 import fi.helsinki.cs.plugin.tmc.services.http.ServerAccess;
+import fi.helsinki.cs.plugin.tmc.ui.UserVisibleException;
 
 public class JsonGetter {
 
-	public String getJson(String url){
-		ServerAccess sa = new ServerAccess();
-		HttpTasks tasks = sa.createHttpTasks();
-		url = sa.getUrl(url);
-		System.out.println(url);
-		HttpRequestExecutor hre =  tasks.createExecutor(url);
+	public String getJson(String url) {
+		ServerAccess request = new ServerAccess();
+		HttpRequestExecutor exec = request.createHttpTasks().createExecutor(request.getUrl(url));
+		
 		try {
-			InputStream is = hre.call().getContent();
-			return IOUtils.toString(is);
-		} catch (Exception e) {
-			System.out.println(e.toString());
-			return "";
+			return IOUtils.toString(exec.call().getContent());
+		} catch(Exception e) {
+			throw new UserVisibleException("Error retrieving JSON data from " + url);
 		}
 	}
-
+	
 }
