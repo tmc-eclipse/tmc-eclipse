9df45899246c1a8416ef675e4e7c140cf63a1229
Nikke S Kostiainen <nkostiai@dat-7.cs.helsinki.fi>
2014-05-19 13:33:37 +0300

    added error handling

---
+++ eclipse-plugin/src/tmc/handlers/EclipseErrorHandler.java
@@ -0,0 +1,23 @@
+package tmc.handlers;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+import fi.helsinki.cs.plugin.tmc.ErrorHandler;
+
+public class EclipseErrorHandler implements ErrorHandler {
+	private Shell parent;
+	
+	
+	public EclipseErrorHandler(Shell parent){
+		this.parent = parent;
+	}
+	
+	@Override
+	public void handleException(Exception e) {
+		MessageDialog dialog = new MessageDialog(parent, "ERROR", null, e.getMessage(), MessageDialog.ERROR, new String[] {"OK"}, 0);
+		dialog.open();
+		parent.close();
+	}
+
+}
---
+++ plugin-logic/src/main/java/fi/helsinki/cs/plugin/tmc/ErrorHandler.java
@@ -0,0 +1,7 @@
+package fi.helsinki.cs.plugin.tmc;
+
+public interface ErrorHandler {
+	
+	public void handleException(Exception e);
+	
+}
