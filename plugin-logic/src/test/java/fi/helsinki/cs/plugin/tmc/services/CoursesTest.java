2d31279504b886256080dcb75800a60eb5ae159d
ljleppan <leo.leppanen@helsinki.fi>
2014-05-21 16:00:20 +0300

    Updated services.Courses tests

--- plugin-logic/src/test/java/fi/helsinki/cs/plugin/tmc/services/CoursesTest.java
+++ plugin-logic/src/test/java/fi/helsinki/cs/plugin/tmc/services/CoursesTest.java
@@ -1,77 +1,103 @@
 package fi.helsinki.cs.plugin.tmc.services;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import fi.helsinki.cs.plugin.tmc.domain.Course;
 import fi.helsinki.cs.plugin.tmc.io.FileIO;
+import fi.helsinki.cs.plugin.tmc.storage.CourseDAO;
 import fi.helsinki.cs.plugin.tmc.storage.LocalCourseStorage;
 import fi.helsinki.cs.plugin.tmc.ui.UserVisibleException;
 
 public class CoursesTest {
-
-	private LocalCourseStorage lcs;
 	private Courses courses;
-	private Course c1;
-	private Course c2;
-	private Course c3;
-	private Course c4;
-	ArrayList<Course> cl;
-	
+	private CourseDAO courseDAO;
+
+	private List<Course> courseList = new ArrayList<Course>();
+	private Course c1 = new Course("c1");
+	private Course c2 = new Course("c2");
+	private Course c3 = new Course("c3");
+
 	@Before
 	public void setUp() throws UserVisibleException {
-		lcs = new LocalCourseStorage(new FileIO("testi.txt"));
-    	courses = new Courses(lcs);
-    	
-    	c1 = new Course("c1");
-    	c2 = new Course("c2");
-    	c3 = new Course("c3");
-    	c4 = new Course("c4");
-    	
-    	cl = new ArrayList<>();
-    	
-    	cl.add(c1);
-    	cl.add(c2);
-    	cl.add(c3);
-    	cl.add(c4);
-    	
-    	courses.setCourses(cl);
+		courseDAO = mock(CourseDAO.class);
+
+		courseList.add(c1);
+		courseList.add(c2);
+		courseList.add(c3);
+
+		when(courseDAO.load()).thenReturn(courseList);
+
+		courses = new Courses(courseDAO);
 	}
-	
 
 	@Test
-	public void testgetCourseByNameSuccess() {
-		assertEquals(courses.getCourseByName("c1"), c1);
+	public void constructorLoadsCourses() {
+		new Courses(courseDAO);
+		verify(courseDAO, times(2)).load(); // first in setup, then here
 	}
-	
+
 	@Test
-	public void testgetCourseByNameFail() {
-		assertNull(courses.getCourseByName("c5"));
+	public void getCoursesReturnCorrectList() {
+		assertEquals(3, courses.getCourses().size());
+		assertTrue(courses.getCourses().contains(c1));
+		assertTrue(courses.getCourses().contains(c2));
+		assertTrue(courses.getCourses().contains(c3));
 	}
-	
+
 	@Test
-	public void testGetCourses() {
-		assertEquals(courses.getCourses(), cl);
+	public void loadCoursesRefreshesFromDAO() {
+		courses.loadCourses();
+		verify(courseDAO, times(2)).load(); // first in setup, then here
 	}
-	
+
 	@Test
-	public void testUpdateCourse() throws UserVisibleException {
-		Course c5 =new Course("c4");
-		c5.setCometUrl("asd");
-		
-		courses.updateCourse(c5);
-		assertEquals(courses.getCourseByName("c4").getCometUrl(), "asd");
+	public void setCourseListSavesToDAO() {
+		List<Course> newList = new ArrayList<Course>();
+		courses.setCourses(newList);
+		verify(courseDAO, times(1)).save(newList);
+		assertEquals(0, courses.getCourses().size());
 	}
-	
+
 	@Test
-	public void testSetCourses() throws UserVisibleException {
-		cl.remove(3);
-		courses.setCourses(cl);
-		assertEquals(courses.getCourses(), cl);
+	public void updateCourseSavesToDAO() {
+		courses.updateCourse(new Course("c1"));
+		verify(courseDAO, times(1)).save(any(List.class));
+	}
+
+	@Test
+	public void updateCourseOverwritesCourseWithSameName() {
+		c1.setId(1337);
+		courses.updateCourse(new Course("c2"));
+		assertTrue(1337 != courses.getCourseByName("c2").getId());
+	}
+
+	@Test
+	public void updateCourseDoesNotOverwriteIfNoCourseWithSameNameIsFound() {
+		courses.updateCourse(new Course("c15"));
+		assertNull(courses.getCourseByName("c15"));
+	}
+
+	@Test
+	public void getCourseByNameReturnsCorrectCourseWhenPresent() {
+		assertEquals(c2, courses.getCourseByName("c2"));
+	}
+
+	@Test
+	public void getCoursesByNameReturnsNullIfNoSuchCoursePresent() {
+		assertNull(courses.getCourseByName("asd"));
+	}
+
+	@Test
+	public void getCoursesByNameReturnsNullIfNoCourses() {
+		courses.setCourses(new ArrayList<Course>());
+		assertNull(courses.getCourseByName("c1"));
 	}
 
 }
