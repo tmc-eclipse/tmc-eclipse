0b8d2227c7752929f1267a967b2fff7dd3057eae
ljleppan <leo.leppanen@helsinki.fi>
2014-05-21 16:47:32 +0300

    Refactored WebDAO to use Dependecy Injection

--- plugin-logic/src/main/java/fi/helsinki/cs/plugin/tmc/ServiceFactory.java
+++ plugin-logic/src/main/java/fi/helsinki/cs/plugin/tmc/ServiceFactory.java
@@ -1,10 +1,13 @@
 package fi.helsinki.cs.plugin.tmc;
 
+import com.google.gson.Gson;
+
 import fi.helsinki.cs.plugin.tmc.io.FileIO;
 import fi.helsinki.cs.plugin.tmc.services.CourseFetcher;
 import fi.helsinki.cs.plugin.tmc.services.Courses;
 import fi.helsinki.cs.plugin.tmc.services.ExerciseFetcher;
 import fi.helsinki.cs.plugin.tmc.services.Settings;
+import fi.helsinki.cs.plugin.tmc.services.web.JsonGetter;
 import fi.helsinki.cs.plugin.tmc.services.web.WebDao;
 import fi.helsinki.cs.plugin.tmc.storage.LocalCourseStorage;
 
@@ -17,9 +20,13 @@ public class ServiceFactory {
 	private CourseFetcher courseFetcher;
 	private ExerciseFetcher exerciseFetcher;
 	private WebDao webDAO;
+	private JsonGetter jsonGetter;
+	private Gson gson;
 	
 	public ServiceFactory() {
-		this.webDAO = new WebDao();
+		this.jsonGetter = new JsonGetter();
+		this.gson = new Gson();
+		this.webDAO = new WebDao(gson, jsonGetter);
 		this.settings = Settings.getDefaultSettings();
 		this.courses = new Courses(new LocalCourseStorage(new FileIO(LOCAL_COURSES_PATH)));
 		this.courseFetcher = new CourseFetcher(courses, webDAO);
