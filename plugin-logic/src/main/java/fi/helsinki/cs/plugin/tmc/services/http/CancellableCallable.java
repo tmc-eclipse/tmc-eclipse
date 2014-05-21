c1e1e678d8a3375ce2cc9851be2d5766db16d458
veepee <villepekkah@gmail.com>
2014-05-21 13:07:10 +0300

    Remove openide dependency

--- plugin-logic/src/main/java/fi/helsinki/cs/plugin/tmc/services/http/CancellableCallable.java
+++ plugin-logic/src/main/java/fi/helsinki/cs/plugin/tmc/services/http/CancellableCallable.java
@@ -2,9 +2,8 @@ package fi.helsinki.cs.plugin.tmc.services.http;
 
 import java.util.concurrent.Callable;
 
-import org.openide.util.Cancellable;
+public interface CancellableCallable<V> extends Callable<V>{
 
-
-public interface CancellableCallable<V> extends  Callable<V>, Cancellable{
-
+    public boolean cancel();
+    
 }
