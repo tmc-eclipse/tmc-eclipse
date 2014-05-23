ca4ab01d3defbebfd9f744e530fe8dc2e1c897f5
Juhani P Helio <juhani.helio@helsinki.fi>
2014-05-23 11:56:06 +0300

    fix?

--- eclipse-plugin/src/tmc/handlers/TestRunnerHandler.java
+++ eclipse-plugin/src/tmc/handlers/TestRunnerHandler.java
@@ -3,30 +3,19 @@ package tmc.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.handlers.HandlerUtil;
-
-import tmc.ui.TestRunnerView;
 
 public class TestRunnerHandler extends AbstractHandler {
 
     public Object execute(ExecutionEvent event) throws ExecutionException {
-
-        IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-
-        // TestRunnerView.setShell(window.getShell());
-
-        TestRunnerView dialog = new TestRunnerView();
 
         try {
             PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
                     .showView("fi.helsinki.cs.plugins.eclipse.views.tmcTestUi");
 
         } catch (PartInitException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+
         }
 
         return null;
