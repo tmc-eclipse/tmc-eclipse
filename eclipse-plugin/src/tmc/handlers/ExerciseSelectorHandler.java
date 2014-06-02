aec35c72e5f1950104e94e17d55ce584ec966bc1
veepee <villepekkah@gmail.com>
2014-06-02 13:59:00 +0300

    Revert changes to ExerciseSelectorHandler

--- eclipse-plugin/src/tmc/handlers/ExerciseSelectorHandler.java
+++ eclipse-plugin/src/tmc/handlers/ExerciseSelectorHandler.java
@@ -3,22 +3,18 @@ package tmc.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.SWT;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import tmc.ui.SuccesfulSubmitDialog;
+import tmc.ui.ExerciseSelectorDialog;
 
 public class ExerciseSelectorHandler extends AbstractHandler {
 
     public Object execute(ExecutionEvent event) throws ExecutionException {
         IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 
-        SuccesfulSubmitDialog dialog = new SuccesfulSubmitDialog(window.getShell());
-        dialog.setPointsAwarded(15);
-        dialog.setModelSolutionUrl("http://google.fi");
-
-        // ExerciseSelectorDialog dialog = new
-        // ExerciseSelectorDialog(window.getShell(), SWT.SHEET);
+        ExerciseSelectorDialog dialog = new ExerciseSelectorDialog(window.getShell(), SWT.SHEET);
         dialog.open();
 
         return null;
