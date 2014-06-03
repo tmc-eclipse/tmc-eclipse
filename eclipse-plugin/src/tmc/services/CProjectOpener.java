2127b4c368818f22f0388e93d4b6af920ff8ab96
Nikke S Kostiainen <nikg@tux.fi>
2014-06-03 15:16:24 +0300

    cleaned cprojectopener

--- eclipse-plugin/src/tmc/services/CProjectOpener.java
+++ eclipse-plugin/src/tmc/services/CProjectOpener.java
@@ -25,11 +25,9 @@ import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.SubProgressMonitor;
 
 import fi.helsinki.cs.plugin.tmc.io.FileUtil;
@@ -48,8 +46,7 @@ public class CProjectOpener {
 			OperationCanceledException, CoreException {
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IWorkspaceRoot root = workspace.getRoot();
-		IProgressMonitor progressMonitor = new NullProgressMonitor();
-		//
+
 		IProject project = root.getProject(projectName);
 		IndexerPreferences.set(project, IndexerPreferences.KEY_INDEXER_ID,
 				IPDOMManager.ID_NO_INDEXER);
@@ -57,146 +54,17 @@ public class CProjectOpener {
 		IProjectDescription description = workspace
 				.newProjectDescription(projectName);
 
-		description
-				.setLocationURI(new File(FileUtil.getNativePath(projectPath))
-						.toURI());
+		description.setLocationURI(new URI(projectPath));
 
-		IPath projectLocation = description.getLocation();
+		project = CCorePlugin.getDefault().createCProject(description, project,
+				new NullProgressMonitor(), projectName);
 
-		if (project.exists()) {
-			project.delete(true, true, null);
-		}
+		MakeProjectNature.addNature(project, new NullProgressMonitor());
 
-		if ((projectLocation != null)
-				&& (!projectLocation.equals(Platform.getLocation()))) {
-			description.setLocation(projectLocation);
+		MakeProjectNature.addToBuildSpec(project, "CBuilder",
+				new NullProgressMonitor());
 
-			CCorePlugin.getDefault().createCProject(description, project, null,
-					MakeCorePlugin.MAKE_PROJECT_ID);
-			IManagedBuildInfo info = ManagedBuildManager
-					.createBuildInfo(project);
-			info.setValid(true);
-			ManagedCProjectNature.addManagedNature(project, null);
-			ManagedCProjectNature.addManagedBuilder(project, null);
+		project.open(new NullProgressMonitor());
 
-			MakeProjectNature.addNature(project, progressMonitor);
-
-			project.open(progressMonitor);
-
-		}
-
-		// @SuppressWarnings("deprecation")
-		// public void importAndOpen() throws URISyntaxException,
-		// OperationCanceledException, CoreException {
-		// IProgressMonitor monitor = new NullProgressMonitor();
-		// IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		// IWorkspaceRoot root = workspace.getRoot();
-		// System.out.println("-1");
-		// //
-		// IProject project = root.getProject(projectName);
-		// IndexerPreferences.set(project, IndexerPreferences.KEY_INDEXER_ID,
-		// IPDOMManager.ID_NO_INDEXER);
-		//
-		// IProjectDescription description = workspace
-		// .newProjectDescription(projectName);
-		//
-		// description
-		// .setLocationURI(new File(FileUtil.getNativePath(projectPath))
-		// .toURI());
-		//
-		// project = CCorePlugin.getDefault().createCProject(description,
-		// project,
-		// new NullProgressMonitor(), MakeCorePlugin.MAKE_PROJECT_ID);
-		//
-		// if (!project.hasNature(CProjectNature.C_NATURE_ID)) {
-		// CProjectNature.addCNature(project, monitor);
-		// }
-		//
-		// if (!project.hasNature(MakeProjectNature.NATURE_ID)) {
-		// MakeProjectNature.addNature(project, new SubProgressMonitor(
-		// monitor, 1));
-		// }
-		//
-		// // ManagedCProjectNature.addManagedNature(project, monitor);
-		//
-		// ScannerConfigNature.addScannerConfigNature(project);
-		//
-		// CCorePlugin.getDefault().mapCProjectOwner(project, projectName,
-		// true);
-		// //
-		// // project.open(new NullProgressMonitor());
-		// System.out.println("0");
-		// try {
-		//
-		// ManagedCProjectNature.addManagedNature(project,
-		// new SubProgressMonitor(monitor, 1));
-		// System.out.println("1");
-		// CCorePlugin.getDefault().mapCProjectOwner(project,
-		// parse(projectName), true);
-		// System.out.println("2");
-		//
-		// IManagedProject newManagedProject = null;
-		// IManagedBuildInfo info = null;
-		//
-		// try {
-		// info = ManagedBuildManager.createBuildInfo(project);
-		// IProjectType projectType = ManagedBuildManager
-		// .getExtensionProjectType(parse(projectName));
-		// newManagedProject = ManagedBuildManager.createManagedProject(
-		// project, projectType);
-		// System.out.println("2");
-		// if (newManagedProject != null) {
-		// IConfiguration[] selectedConfigs = projectType
-		// .getConfigurations();
-		// for (int i = 0; i < selectedConfigs.length; i++) {
-		// IConfiguration config = selectedConfigs[i];
-		// int id = ManagedBuildManager.getRandomNumber();
-		// IConfiguration newConfig = newManagedProject
-		// .createConfiguration(config, config.getId()
-		// + "." + id);
-		// newConfig.setArtifactName(project.getName());
-		// }
-		// System.out.println("3");
-		// IConfiguration[] newConfigs = newManagedProject
-		// .getConfigurations();
-		// IConfiguration defaultCfg = null;
-		// for (int i = 0; i < newConfigs.length; i++) {
-		// if (newConfigs[i].isSupported()) {
-		// defaultCfg = newConfigs[i];
-		// break;
-		// }
-		// }
-		// System.out.println("4");
-		//
-		// if (defaultCfg == null && newConfigs.length > 0)
-		// defaultCfg = newConfigs[0];
-		//
-		// if (defaultCfg != null) {
-		// ManagedBuildManager.setDefaultConfiguration(project,
-		// defaultCfg);
-		// ManagedBuildManager.setSelectedConfiguration(project,
-		// defaultCfg);
-		// }
-		// ManagedBuildManager.setNewProjectVersion(project);
-		// }
-		// }
-		//
-		// catch (BuildException e) {
-		// e.printStackTrace();
-		//
-		// }
-		// System.out.println("5");
-		// if (info != null) {
-		// info.setValid(true);
-		// ManagedBuildManager.saveBuildInfo(project, true);
-		// }
-		//
-		// } finally {
-		// project.open(monitor);
-		// }
-		// }
-		//
-		// public String parse(String string) {
-		// return string.replaceAll("-", "//");
 	}
 }
