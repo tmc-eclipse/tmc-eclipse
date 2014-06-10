0ae29fcc965f778d23d4968d30fd6f617d941cae
veepee <villepekkah@gmail.com>
2014-06-10 11:44:04 +0300

    Only prevent overwriting files in src

--- plugin-logic/src/main/java/fi/helsinki/cs/plugin/tmc/io/zipper/unzippingdecider/DefaultUnzippingDecider.java
+++ plugin-logic/src/main/java/fi/helsinki/cs/plugin/tmc/io/zipper/unzippingdecider/DefaultUnzippingDecider.java
@@ -1,20 +1,20 @@
 package fi.helsinki.cs.plugin.tmc.io.zipper.unzippingdecider;
 
 import fi.helsinki.cs.plugin.tmc.domain.Project;
+import fi.helsinki.cs.plugin.tmc.io.FileIO;
 
 public class DefaultUnzippingDecider extends AbstractUnzippingDecider {
 
     public DefaultUnzippingDecider(Project project) {
-    	super(project);
+        super(project);
     }
 
     @Override
     public boolean shouldUnzip(String filePath) {
-		String s = project.getRootPath() + "/src";
-		if (filePath.startsWith(s)
-				&& (filePath.equals(s) || filePath.charAt(s.length())  == '/')) {
-			return false;
-		}
+        String s = project.getRootPath() + "/src";
+        if (filePath.startsWith(s) && (filePath.equals(s) || filePath.charAt(s.length()) == '/')) {
+            return !(new FileIO(filePath).fileExists());
+        }
         return super.shouldUnzip(filePath);
     }
 
