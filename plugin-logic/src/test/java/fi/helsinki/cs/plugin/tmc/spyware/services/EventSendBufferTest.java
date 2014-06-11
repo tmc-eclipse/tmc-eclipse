2b8b3b077457abbc07a1a1914185c401114cef20
Juhani P Helio <juhani.helio@helsinki.fi>
2014-06-11 15:17:10 +0300

    made some useless tests :p

---
+++ plugin-logic/src/test/java/fi/helsinki/cs/plugin/tmc/spyware/services/EventSendBufferTest.java
@@ -0,0 +1,17 @@
+package fi.helsinki.cs.plugin.tmc.spyware.services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventSendBufferTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
