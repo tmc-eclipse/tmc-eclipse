396a2710c88fba945f9e8259f60d3a4f8298c595
veepee <villepekkah@gmail.com>
2014-06-12 10:49:30 +0300

    Save project database on IDE exit

--- eclipse-plugin/src/tmc/activator/CoreInitializer.java
+++ eclipse-plugin/src/tmc/activator/CoreInitializer.java
@@ -39,7 +39,7 @@ public class CoreInitializer extends AbstractUIPlugin implements IStartup {
 
     public void stop(BundleContext context) throws Exception {
         Core.getProjectDAO().save();
-        
+
         instance = null;
         super.stop(context);
     }
