5a79f0c4d99734de769e217a25395bf3e85227ff
unknown <nikg@tux.fi>
2014-06-12 13:26:13 +0300

    fixed uploadhandler

--- eclipse-plugin/src/tmc/handlers/UploadHandler.java
+++ eclipse-plugin/src/tmc/handlers/UploadHandler.java
@@ -9,12 +9,14 @@ import org.eclipse.ui.handlers.HandlerUtil;
 
 import fi.helsinki.cs.plugin.tmc.Core;
 import tmc.activator.CoreInitializer;
+import tmc.handlers.listeners.SelectionListener;
 import tmc.tasks.TaskStarter;
 import tmc.ui.EclipseIdeUIInvoker;
 import tmc.util.WorkbenchHelper;
 
 public class UploadHandler extends AbstractHandler {
     WorkbenchHelper helper;
+    SelectionListener listener;
 
     public UploadHandler() {
         this.helper = CoreInitializer.getDefault().getWorkbenchHelper();
@@ -26,12 +28,16 @@ public class UploadHandler extends AbstractHandler {
     public Object execute(ExecutionEvent event) throws ExecutionException {
         if (helper.saveOpenFiles()) {
             Shell shell = HandlerUtil.getActiveWorkbenchWindowChecked(event).getShell();
-            if (helper.getActiveProject().getExercise().hasDeadlinePassed()) {
-                Core.getErrorHandler().handleManualException("The deadline for this exercise has passed.");
+            // TODO: Replace this functionality with disabling the submit button
+            // when appropriate
+            if (helper.getActiveProject() == null || helper.getActiveProject().getExercise().hasDeadlinePassed()) {
+                Core.getErrorHandler().handleManualException(
+                        "No valid exercise active or the deadline for this exercise has passed.");
             } else {
                 TaskStarter.startExerciseUploadTask(new EclipseIdeUIInvoker(shell));
             }
         }
         return null;
     }
+
 }
