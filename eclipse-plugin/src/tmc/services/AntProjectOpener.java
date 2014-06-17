37399e06fcc85fb257e53d6b6ab704f0efb48c2e
unknown <nikg@tux.fi>
2014-06-17 11:43:36 +0300

    Refactored project openers

--- eclipse-plugin/src/tmc/services/AntProjectOpener.java
+++ eclipse-plugin/src/tmc/services/AntProjectOpener.java
@@ -16,26 +16,19 @@ public class AntProjectOpener {
         this.pathToProjectFile = pathToProjectFile;
     }
 
-    public void importAndOpen() throws CoreException {
+    public IProject importAndOpen() throws CoreException {
         IProjectDescription description = ResourcesPlugin.getWorkspace().loadProjectDescription(
                 new Path(pathToProjectFile));
         IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(description.getName());
 
-        project.create(description, null);
+        if (!project.exists()) {
+            project.create(description, null);
 
-        project.open(new NullProgressMonitor());
+            project.open(new NullProgressMonitor());
 
-        description = project.getDescription();
+        }
 
-        String[] prevNatures = description.getNatureIds();
-        String[] newNatures = new String[prevNatures.length + 1];
-        System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
-        newNatures[prevNatures.length] = TMCProjectNature.NATURE_ID;
-        String temp = newNatures[newNatures.length - 1];
-        newNatures[newNatures.length - 1] = newNatures[0];
-        newNatures[0] = temp;
-        description.setNatureIds(newNatures);
-        project.setDescription(description, new NullProgressMonitor());
+        return project;
 
     }
 
