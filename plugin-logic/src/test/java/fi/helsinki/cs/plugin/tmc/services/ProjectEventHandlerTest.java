480cdf75ee7ba2e3549116cb21dd399ebe6a26d2
Erkka O Kaaria <erkka_kaaria@hotmail.com>
2014-06-17 10:23:37 +0300

    Fixed broken test

--- plugin-logic/src/test/java/fi/helsinki/cs/plugin/tmc/services/ProjectEventHandlerTest.java
+++ plugin-logic/src/test/java/fi/helsinki/cs/plugin/tmc/services/ProjectEventHandlerTest.java
@@ -1,17 +1,26 @@
 package fi.helsinki.cs.plugin.tmc.services;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
-import fi.helsinki.cs.plugin.tmc.domain.Exercise;
 import fi.helsinki.cs.plugin.tmc.domain.Project;
-import fi.helsinki.cs.plugin.tmc.io.ProjectScanner;
+import fi.helsinki.cs.plugin.tmc.domain.ProjectStatus;
 import fi.helsinki.cs.plugin.tmc.spyware.ChangeType;
 import fi.helsinki.cs.plugin.tmc.spyware.SnapshotInfo;
-import static org.mockito.Mockito.*;
 
 public class ProjectEventHandlerTest {
 
@@ -25,18 +34,24 @@ public class ProjectEventHandlerTest {
         projectDAO = mock(ProjectDAO.class);
         project = mock(Project.class);
 
+        ArrayList<String> testExercise1Files = new ArrayList<String>();
+        testExercise1Files.add("testCourse/testExercise1/existingFile");
+
+        when(project.getReadOnlyProjectFiles()).thenReturn(testExercise1Files);
+
         when(projectDAO.getProjectByFile("testCourse/testExercise1/existingFile")).thenReturn(project);
         when(projectDAO.getProjectByFile("testCourse/testExercise1/newFile")).thenReturn(project);
 
         when(project.containsFile("testCourse/testExercise1/nonExistingFile")).thenReturn(false);
         when(project.containsFile("testCourse/testExercise1/existingFile")).thenReturn(true);
+
         when(project.getRootPath()).thenReturn("testCourse/testExercise1");
 
         handler = new ProjectEventHandler(projectDAO);
     }
 
     @Test
-    public void testHandleWithInvalidFile() {
+    public void testHandleSnapshotRenameWithInvalidFile() {
         SnapshotInfo snapshot = new SnapshotInfo("project", "", "", "testCourse/testExercise1/nonExistingFile", "",
                 ChangeType.FILE_RENAME);
 
@@ -47,6 +62,25 @@ public class ProjectEventHandlerTest {
 
         verify(project, never()).getReadOnlyProjectFiles();
         verify(project, never()).setProjectFiles(any(List.class));
+    }
+
+    @Test
+    public void testHandleDeletionWithValidProject() {
+        Mockito.doAnswer(new Answer() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                List<String> param = (List<String>) invocation.getArguments()[0];
+                assertEquals(0, param.size());
+                return null;
+            }
+
+        }).when(project).setProjectFiles(Mockito.anyListOf(String.class));
+        String filePath = "testCourse/testExercise1";
+        when(projectDAO.getProjectByFile(filePath)).thenReturn(project);
+        handler.handleDeletion(filePath);
+        verify(project, times(1)).setStatus(ProjectStatus.DELETED);
+
     }
 
     @Test
