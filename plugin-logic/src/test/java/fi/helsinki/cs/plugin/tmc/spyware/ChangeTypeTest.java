00763001b6327337fe5f60076c24600e14f9270d
ljleppan <leo.leppanen@helsinki.fi>
2014-06-19 09:57:41 +0300

    Some tests for Spyware

---
+++ plugin-logic/src/test/java/fi/helsinki/cs/plugin/tmc/spyware/ChangeTypeTest.java
@@ -0,0 +1,26 @@
+package fi.helsinki.cs.plugin.tmc.spyware;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ChangeTypeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void requiredEnumsExist() {
+        assertNotNull(ChangeType.NONE);
+        assertNotNull(ChangeType.FILE_CREATE);
+        assertNotNull(ChangeType.FILE_RENAME);
+        assertNotNull(ChangeType.FILE_CHANGE);
+        assertNotNull(ChangeType.FILE_DELETE);
+        assertNotNull(ChangeType.FOLDER_CREATE);
+        assertNotNull(ChangeType.FOLDER_DELETE);
+        assertNotNull(ChangeType.FOLDER_RENAME);
+    }
+
+}
