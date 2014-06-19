65dde26107d2ffbcf58a44da3a57aaf45ca60013
unknown <nikg@tux.fi>
2014-06-19 12:38:18 +0300

    issue 32 fixed

--- eclipse-plugin/src/tmc/services/GenericProjectOpener.java
+++ eclipse-plugin/src/tmc/services/GenericProjectOpener.java
@@ -6,6 +6,8 @@ import java.net.URISyntaxException;
 import org.eclipse.core.internal.resources.ResourceException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
@@ -33,35 +35,16 @@ public class GenericProjectOpener implements ProjectOpener {
 
     public void open(Exercise e) {
 
-        System.out.println("OPEN CALLED");
         Project project = projectDAO.getProjectByExercise(e);
         ProjectType projectType = project.getProjectType();
         IProject projectObject = null;
         if (!IProjectHelper.projectWithThisFilePathExists(project.getRootPath())) {
 
-            try {
-                switch (project.getProjectType()) {
-                case JAVA_ANT:
-                    projectObject = openAntProject(project, projectType);
-                    break;
-                case JAVA_MAVEN:
-                    projectObject = openMavenProject(project, projectType);
-                    break;
-                case MAKEFILE:
-                    projectObject = openCProject(project, projectType);
-                    break;
-                default:
-                    break;
-                }
-
-            } catch (Exception exception) {
-                errorHandler.handleException(exception);
-            }
+            projectObject = openWithCorrectOpener(project, projectType, projectObject);
         } else {
             projectObject = IProjectHelper.getProjectWithThisFilePath(project.getRootPath());
         }
         try {
-            System.out.println(projectObject.getName());
             addTMCProjectNature(projectObject);
         } catch (CoreException e1) {
             Core.getErrorHandler().handleException(e1);
@@ -69,26 +52,57 @@ public class GenericProjectOpener implements ProjectOpener {
 
     }
 
+    private IProject openWithCorrectOpener(Project project, ProjectType projectType, IProject projectObject) {
+        try {
+            switch (project.getProjectType()) {
+            case JAVA_ANT:
+                projectObject = openAntProject(project, projectType);
+                break;
+            case JAVA_MAVEN:
+                projectObject = openMavenProject(project, projectType);
+                break;
+            case MAKEFILE:
+                projectObject = openCProject(project, projectType);
+                break;
+            default:
+                break;
+            }
+
+        } catch (Exception exception) {
+            errorHandler.handleException(exception);
+        }
+        return projectObject;
+    }
+
     private void addTMCProjectNature(IProject projectObject) throws CoreException {
         IProjectDescription description = projectObject.getDescription();
 
-        String[] prevNatures = description.getNatureIds();
-        String[] newNatures = new String[prevNatures.length + 1];
-        System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
-        newNatures[prevNatures.length] = TMCProjectNature.NATURE_ID;
-        String temp = newNatures[newNatures.length - 1];
-        newNatures[newNatures.length - 1] = newNatures[0];
-        newNatures[0] = temp;
-        description.setNatureIds(newNatures);
+        setNaturesToCorrectPosition(description);
+
         try {
             projectObject.setDescription(description, new NullProgressMonitor());
         } catch (ResourceException re) {
             // A valid project description already exists, no need to do
             // anything
         }
-        
-        for(String s: projectObject.getDescription().getNatureIds()){
-            System.out.println(s);
+
+    }
+
+    private void setNaturesToCorrectPosition(IProjectDescription description) {
+        try {
+            ResourcesPlugin.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+        } catch (CoreException e) {
+            Core.getErrorHandler().handleException(e);
+        }
+        if (!description.hasNature(TMCProjectNature.NATURE_ID)) {
+            String[] prevNatures = description.getNatureIds();
+            String[] newNatures = new String[prevNatures.length + 1];
+            System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
+            newNatures[prevNatures.length] = TMCProjectNature.NATURE_ID;
+            String temp = newNatures[newNatures.length - 1];
+            newNatures[newNatures.length - 1] = newNatures[0];
+            newNatures[0] = temp;
+            description.setNatureIds(newNatures);
         }
     }
 
@@ -109,15 +123,6 @@ public class GenericProjectOpener implements ProjectOpener {
     private IProject openAntProject(Project project, ProjectType projectType) throws CoreException {
         return new AntProjectOpener(FileUtil.append(project.getRootPath(), ".project")).importAndOpen();
 
-    }
-
-    private boolean hasTMCNature(IProject project) throws CoreException {
-        for (String s : project.getDescription().getNatureIds()) {
-            if (s.equals(TMCProjectNature.NATURE_ID)) {
-                return true;
-            }
-        }
-        return false;
     }
 
 }
