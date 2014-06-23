695cba52027b2b6acc64a935346bfe76af092880
veepee <villepekkah@gmail.com>
2014-06-23 11:02:29 +0300

    Do not open completed exercises dialog if no exercises are available

--- eclipse-plugin/src/tmc/handlers/CompletedExerciseSelectorHandler.java
+++ eclipse-plugin/src/tmc/handlers/CompletedExerciseSelectorHandler.java
@@ -8,15 +8,20 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import tmc.ui.ExerciseSelectorDialog;
+import fi.helsinki.cs.plugin.tmc.Core;
 
 public class CompletedExerciseSelectorHandler extends AbstractHandler {
 
     public Object execute(ExecutionEvent event) throws ExecutionException {
         IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 
-        ExerciseSelectorDialog dialog = new ExerciseSelectorDialog(window.getShell(), SWT.SHEET);
-        dialog.setShowCompleted(true);
-        dialog.open();
+        if (!Core.getCourseDAO().getCurrentCourse(Core.getSettings()).getCompletedDownloadableExercises().isEmpty()) {
+            ExerciseSelectorDialog dialog = new ExerciseSelectorDialog(window.getShell(), SWT.SHEET);
+            dialog.setShowCompleted(true);
+            dialog.open();
+        } else {
+            Core.getErrorHandler().raise("No exercises to be downloaded/updated");
+        }
 
         return null;
     }
