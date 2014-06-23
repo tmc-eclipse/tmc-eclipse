8a86f20714d1b717a345cf12b60160085544bd76
Juhani P Helio <juhani.helio@helsinki.fi>
2014-06-23 15:42:49 +0300

    small fix in exerciseSelectorHandler

--- eclipse-plugin/src/tmc/handlers/ExerciseSelectorHandler.java
+++ eclipse-plugin/src/tmc/handlers/ExerciseSelectorHandler.java
@@ -10,15 +10,20 @@ import org.eclipse.ui.handlers.HandlerUtil;
 import tmc.ui.ExerciseSelectorDialog;
 import fi.helsinki.cs.plugin.tmc.Core;
 import fi.helsinki.cs.plugin.tmc.domain.Course;
+import fi.helsinki.cs.plugin.tmc.ui.UserVisibleException;
 
 public class ExerciseSelectorHandler extends AbstractHandler {
 
     public Object execute(ExecutionEvent event) throws ExecutionException {
         IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 
-        Course currentCourse = Core.getCourseDAO().getCurrentCourse(Core.getSettings());
-        Core.getUpdater().updateExercises(currentCourse);
-
+        try {
+            Course currentCourse = Core.getCourseDAO().getCurrentCourse(Core.getSettings());
+            Core.getUpdater().updateExercises(currentCourse);
+        } catch (UserVisibleException uve) {
+            Core.getErrorHandler().raise(uve.getLocalizedMessage());
+            return null;
+        }
         if (!Core.getCourseDAO().getCurrentCourse(Core.getSettings()).getDownloadableExercises().isEmpty()) {
             ExerciseSelectorDialog dialog = new ExerciseSelectorDialog(window.getShell(), SWT.SHEET);
             dialog.setShowCompleted(false);
