92ccda356bff5b89e4811db4224b2e70cb5cc91c
Erkka O Kaaria <erkka_kaaria@hotmail.com>
2014-06-24 13:00:36 +0300

    Added more tests for Updater

--- plugin-core/src/test/java/fi/helsinki/cs/tmc/core/services/UpdaterTest.java
+++ plugin-core/src/test/java/fi/helsinki/cs/tmc/core/services/UpdaterTest.java
@@ -1,5 +1,7 @@
 package fi.helsinki.cs.tmc.core.services;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -14,7 +16,9 @@ import org.junit.Test;
 
 import fi.helsinki.cs.tmc.core.domain.Course;
 import fi.helsinki.cs.tmc.core.domain.Exercise;
+import fi.helsinki.cs.tmc.core.domain.Project;
 import fi.helsinki.cs.tmc.core.services.http.ServerManager;
+import fi.helsinki.cs.tmc.core.ui.UserVisibleException;
 
 public class UpdaterTest {
 
@@ -73,4 +77,194 @@ public class UpdaterTest {
         verify(course, times(1)).getExercises();
 
     }
+
+    @Test
+    public void newCourseExercisesAreSetCorrectlyAsOldExercises() {
+
+        List<Course> serverCourseList = new ArrayList<Course>();
+        Course serverCourse = new Course("course_name");
+        serverCourseList.add(serverCourse);
+        when(server.getCourses()).thenReturn(serverCourseList);
+
+        List<Course> daoCourseList = new ArrayList<Course>();
+        Course daoCourse = new Course("course_name");
+
+        List<Exercise> exercises = new ArrayList<Exercise>();
+        exercises.add(new Exercise("foo", "bar"));
+        exercises.get(0).setCourse(daoCourse);
+        daoCourse.setExercises(exercises);
+        daoCourseList.add(daoCourse);
+
+        when(courseDAO.getCourses()).thenReturn(daoCourseList);
+
+        updater.updateCourses();
+        assertEquals(exercises, serverCourse.getExercises());
+    }
+
+    @Test
+    public void newCourseExercisesHaveCorrectParentCourse() {
+
+        List<Course> serverCourseList = new ArrayList<Course>();
+        Course serverCourse = new Course("course_name");
+        serverCourseList.add(serverCourse);
+        when(server.getCourses()).thenReturn(serverCourseList);
+
+        List<Course> daoCourseList = new ArrayList<Course>();
+        Course daoCourse = new Course("course_name");
+
+        List<Exercise> exercises = new ArrayList<Exercise>();
+        exercises.add(new Exercise("foo", "bar"));
+        exercises.get(0).setCourse(daoCourse);
+        daoCourse.setExercises(exercises);
+        daoCourseList.add(daoCourse);
+
+        when(courseDAO.getCourses()).thenReturn(daoCourseList);
+
+        updater.updateCourses();
+
+        assertTrue(exercises.get(0).getCourse() == serverCourse);
+    }
+
+    @Test
+    public void newCourseExercisesAreNotOverriddenIfOldCourseExercisesAreNull() {
+
+        List<Course> serverCourseList = new ArrayList<Course>();
+        Course serverCourse = new Course("course_name");
+        serverCourseList.add(serverCourse);
+        when(server.getCourses()).thenReturn(serverCourseList);
+
+        List<Course> daoCourseList = new ArrayList<Course>();
+        Course daoCourse = new Course("course_name");
+        daoCourseList.add(daoCourse);
+        daoCourse.setExercises(null);
+        List<Exercise> exercises = new ArrayList<Exercise>();
+
+        exercises.add(new Exercise("foo", "bar"));
+        exercises.get(0).setCourse(serverCourse);
+
+        serverCourse.setExercises(exercises);
+
+        when(courseDAO.getCourses()).thenReturn(daoCourseList);
+
+        updater.updateCourses();
+
+        assertEquals(exercises, serverCourse.getExercises());
+    }
+
+    @Test
+    public void newCourseExercisesAreNotOverriddenIfOldCourseIsNotEqual() {
+
+        List<Course> serverCourseList = new ArrayList<Course>();
+        Course serverCourse = new Course("course_name");
+        serverCourseList.add(serverCourse);
+        when(server.getCourses()).thenReturn(serverCourseList);
+
+        List<Course> daoCourseList = new ArrayList<Course>();
+        Course daoCourse = new Course("different_course_name");
+        daoCourseList.add(daoCourse);
+
+        List<Exercise> exercises = new ArrayList<Exercise>();
+
+        exercises.add(new Exercise("foo", "bar"));
+        exercises.get(0).setCourse(serverCourse);
+
+        serverCourse.setExercises(exercises);
+
+        when(courseDAO.getCourses()).thenReturn(daoCourseList);
+
+        updater.updateCourses();
+
+        assertEquals(exercises, serverCourse.getExercises());
+    }
+
+    @Test(expected = UserVisibleException.class)
+    public void exceptionIsThrownIfCourseIsNull() {
+        updater.updateExercises(null);
+    }
+
+    @Test
+    public void exerciseIsUpdatedWhenThereIsOldExerciseForCourse() {
+        Project project = mock(Project.class);
+
+        Course course = new Course("foo");
+        course.setId(1);
+
+        List<Exercise> exercises = new ArrayList<Exercise>();
+        exercises.add(new Exercise("foo", "bar"));
+        exercises.add(new Exercise("baz", "qux"));
+        exercises.get(1).setChecksum("1234");
+        exercises.get(1).setUpdated(false);
+        exercises.get(1).setCourse(course);
+        exercises.get(1).setProject(project);
+
+        course.setExercises(exercises);
+
+        List<Exercise> serverExercises = new ArrayList<Exercise>();
+        serverExercises.add(new Exercise("baz", "qux"));
+        when(server.getExercises("1")).thenReturn(serverExercises);
+
+        updater.updateExercises(course);
+        assertEquals(exercises.get(1).getChecksum(), serverExercises.get(0).getOldChecksum());
+        assertEquals(exercises.get(1).getUpdated(), serverExercises.get(0).getUpdated());
+        assertEquals(exercises.get(1).getCourse(), serverExercises.get(0).getCourse());
+        assertEquals(exercises.get(1).getProject(), serverExercises.get(0).getProject());
+    }
+
+    @Test
+    public void projectHasCorrectExerciseSet() {
+        Project project = mock(Project.class);
+
+        Course course = new Course("foo");
+        course.setId(1);
+
+        List<Exercise> exercises = new ArrayList<Exercise>();
+        exercises.add(new Exercise("foo", "bar"));
+        exercises.add(new Exercise("baz", "qux"));
+        exercises.get(1).setChecksum("1234");
+        exercises.get(1).setUpdated(false);
+        exercises.get(1).setCourse(course);
+        exercises.get(1).setProject(project);
+
+        course.setExercises(exercises);
+
+        List<Exercise> serverExercises = new ArrayList<Exercise>();
+        serverExercises.add(new Exercise("baz", "qux"));
+
+        when(server.getExercises("1")).thenReturn(serverExercises);
+        when(projectDAO.getProjectByExercise(exercises.get(1))).thenReturn(project);
+
+        updater.updateExercises(course);
+        verify(project, times(1)).setExercise(serverExercises.get(0));
+    }
+
+    @Test
+    public void serverExerciseIsNotModifiedIfNoLocalMatchIsPresent() {
+        Project project = mock(Project.class);
+
+        Course course = new Course("foo");
+        course.setId(1);
+
+        List<Exercise> exercises = new ArrayList<Exercise>();
+        exercises.add(new Exercise("foo", "bar"));
+
+        course.setExercises(exercises);
+
+        List<Exercise> serverExercises = new ArrayList<Exercise>();
+        serverExercises.add(new Exercise("baz", "qux"));
+
+        serverExercises.get(0).setOldChecksum("1234");
+        serverExercises.get(0).setUpdated(false);
+        serverExercises.get(0).setCourse(course);
+        serverExercises.get(0).setProject(project);
+
+        when(server.getExercises("1")).thenReturn(serverExercises);
+
+        updater.updateExercises(course);
+
+        assertEquals("1234", serverExercises.get(0).getOldChecksum());
+        assertEquals(false, serverExercises.get(0).getUpdated());
+        assertEquals(course, serverExercises.get(0).getCourse());
+        assertEquals(project, serverExercises.get(0).getProject());
+    }
+
 }
