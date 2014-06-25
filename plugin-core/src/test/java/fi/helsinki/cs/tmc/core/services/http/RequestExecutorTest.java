b179fe3bad7bba7bb651b1edb56bcc732c8cccc1
Erkka O Kaaria <erkka_kaaria@hotmail.com>
2014-06-25 15:34:58 +0300

    New test for requestexecutor

--- plugin-core/src/test/java/fi/helsinki/cs/tmc/core/services/http/RequestExecutorTest.java
+++ plugin-core/src/test/java/fi/helsinki/cs/tmc/core/services/http/RequestExecutorTest.java
@@ -1,23 +1,36 @@
 package fi.helsinki.cs.tmc.core.services.http;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.Header;
 import org.apache.http.StatusLine;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.params.HttpParams;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import fi.helsinki.cs.tmc.core.services.Settings;
 
@@ -34,6 +47,16 @@ public class RequestExecutorTest {
         StatusLine line = mock(StatusLine.class);
         when(line.getStatusCode()).thenReturn(200);
         when(response.getStatusLine()).thenReturn(line);
+
+        BufferedHttpEntity entity = mock(BufferedHttpEntity.class);
+        InputStream stream = mock(InputStream.class);
+        when(stream.read()).thenReturn(-1);
+        when(stream.read(any(byte[].class))).thenReturn(-1);
+        when(stream.read(any(byte[].class), anyInt(), anyInt())).thenReturn(-1);
+
+        when(entity.getContent()).thenReturn(stream);
+
+        when(response.getEntity()).thenReturn(entity);
         when(client.execute(any(HttpUriRequest.class))).thenReturn(response);
         factory = new MockHttpFactory(client);
     }
@@ -58,18 +81,38 @@ public class RequestExecutorTest {
         assertTrue(executor.toString().contains("username password"));
     }
 
-    @Test(expected = IllegalStateException.class)
-    public void nonExistentUrlCausesException() throws IOException, InterruptedException, FailedHttpResponseException {
-        this.executor = new RequestExecutor("sadasdasd", factory, mock(Settings.class));
-        executor.execute();
-    }
+    @Test
+    public void executorSetsCredentialsToRequest() throws FailedHttpResponseException, IOException,
+            InterruptedException, URISyntaxException {
+        HttpUriRequest request = mock(HttpUriRequest.class);
+        URI uri = new URI("http://foo.com");
+        when(request.getURI()).thenReturn(uri);
 
-    // @Test
-    // public void executorSetsCredentialsToRequest() {
-    // HttpUriRequest request = mock(HttpUriRequest.class);
-    // executor = new RequestExecutor(request, factory);
-    //
-    // executor.setCredentials("foo", "bar");
-    // executor.execute()
-    // }
+        HttpParams param = mock(HttpParams.class);
+        // encoding name, for example "UTF-8"; null seems to work as it picks
+        // some default encoding in this case
+
+        when(param.getParameter(anyString())).thenReturn(null);
+        when(request.getParams()).thenReturn(param);
+
+        executor = new RequestExecutor(request, factory, mock(Settings.class));
+
+        executor.setCredentials("foo", "bar");
+
+        doAnswer(new Answer() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Header header = (Header) invocation.getArguments()[0];
+                String usernamePassword = new String(Base64.decodeBase64(header.getValue().split(" ")[1]));
+                assertEquals("foo:bar", usernamePassword);
+                return null;
+            }
+
+        }).when(request).addHeader(any(Header.class));
+
+        executor.execute();
+
+        verify(request, times(1)).addHeader(any(Header.class));
+    }
 }
